f5bd404ad16fbb81dc9471e2a3d9933c2f239c0b
Linda McGraw <linda.mcgraw@me.com>
2024-09-26 20:55:50 +0000

    commit project work

--- myApp.js
+++ myApp.js
@@ -1,49 +1,36 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require('helmet');
+const seconds = 90 * 24 * 60 * 60;
+// app.use(helmet.hidePoweredBy(),
+//   helmet.frameguard({ action: 'deny' }),
+//   helmet.xssFilter(),
+//   helmet.noSniff(),
+//   helmet.ieNoOpen(),
+//   helmet.hsts({ maxAge: seconds, force: true }),
+//   helmet.dnsPrefetchControl(),
+//   helmet.noCache(),
+//   helmet.contentSecurityPolicy({
+//     directives:
+//     {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'", 'trusted-cdn.com']
+//     }
+//   })
+// );
+app.use(helmet({
+  hsts: { maxAge: seconds, force: true },
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", 'trusted-cdn.com', 'style.com'],
+    }
+  },
+  dnsPrefetchControl: false
+}));
 
 
 
@@ -51,8 +38,7 @@ module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
